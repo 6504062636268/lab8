a56aed35c6dd7f9890d9565fb3564f9541e885fd
PUSANISA CHANNUAN <153055201+6504062636268@users.noreply.github.com>
2026-02-09 16:27:21 +0700

    Lab8no2.c: show number of no.1 stat

--- Lab8no2_2.c
+++ Lab8no2_2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 char charkeys[10]={'D','B','D','C','C','D','A','E','A','D'};
-int checkscore(char std[]);
 int countCorrectQ1(char ans[][10], int students);
 
 int main() {
@@ -14,28 +13,11 @@ int main() {
         	{'B','B','E','C','C','D','E','E','A','D'},
         	{'B','B','A','C','C','D','E','E','A','D'},
         	{'E','B','E','C','C','D','E','E','A','D'}};
-
-    
-   	for(i=0;i<8;i++){
-   		printf("std %d => %d\n", (i+1), checkscore(ans[i]));
-	   }
 	   
 	printf("Number of students correct in question 1 = %d\n",
        countCorrectQ1(ans, 8));
 
     return 0;
-}
-
-int checkscore(char std[]){
-    int score=0;
-    int i;
-
-    for(i=0;i<10;i++){
-        if(std[i] == charkeys[i])
-            score++;
-        
-    }
-    return score;
 }
 
 int countCorrectQ1(char ans[][10],int students){
@@ -47,5 +29,6 @@ int countCorrectQ1(char ans[][10],int students){
     }
     return count;
 }
+
 
 
