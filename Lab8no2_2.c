bca259a28abc9a8cf73e175f385d8036a2f33ccb
PUSANISA CHANNUAN <153055201+6504062636268@users.noreply.github.com>
2026-02-09 16:21:09 +0700

    Lab8no2.c: show number of no.1 stat

---
+++ Lab8no2_2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+char charkeys[10]={'D','B','D','C','C','D','A','E','A','D'};
+int checkscore(char std[]);
+int countCorrectQ1(char ans[][10], int students);
+
+int main() {
+    int i,j;
+    char ans[8][10]={
+        	{'A','B','A','C','C','D','E','E','A','D'},
+        	{'D','B','A','B','C','A','E','E','A','D'},
+        	{'E','D','D','A','C','B','E','E','A','D'},
+        	{'C','B','A','E','D','C','E','E','A','D'},
+        	{'A','B','D','C','C','D','E','E','A','D'},
+        	{'B','B','E','C','C','D','E','E','A','D'},
+        	{'B','B','A','C','C','D','E','E','A','D'},
+        	{'E','B','E','C','C','D','E','E','A','D'}};
+
+    
+   	for(i=0;i<8;i++){
+   		printf("std %d => %d\n", (i+1), checkscore(ans[i]));
+	   }
+	   
+	printf("Number of students correct in question 1 = %d\n",
+       countCorrectQ1(ans, 8));
+
+    return 0;
+}
+
+int checkscore(char std[]){
+    int score=0;
+    int i;
+
+    for(i=0;i<10;i++){
+        if(std[i] == charkeys[i])
+            score++;
+        
+    }
+    return score;
+}
+
+int countCorrectQ1(char ans[][10],int students){
+    int count=0;
+    int i;
+    for(i=0; i<students;i++){
+        if(ans[i][0]==charkeys[0])
+            count++;
+    }
+    return count;
+}
+
+
